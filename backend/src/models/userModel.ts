f34aab04460dc9c1787d61bec08af7221bb3b4f7
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2024-09-15 20:02:02 +0530

    add: create userModel schema
    - Added IUser user interface definition in userModel.ts
    - Added userModel schema definition in userModel.ts

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,46 @@
+import { Schema, model, Document } from "mongoose";
+
+interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    isVerified: boolean;
+    hostedEvents: Schema.Types.ObjectId[];
+    joinedEvents: Schema.Types.ObjectId[];
+    createdAt: Date;
+}
+
+const userSchema = new Schema<IUser>({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    hostedEvents: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Event'
+    },
+    joinedEvents: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Event'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+const User = model('User', userSchema);
+export default User;
