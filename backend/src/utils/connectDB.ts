11ff8317b86e934fb0bd3ef41e682e064db98878
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2024-09-15 18:45:40 +0530

    Create connectDB utility for database connection
    - Added database connection logic in connectDB.ts with error handling

---
+++ backend/src/utils/connectDB.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const MONGO_URI: string = process.env.MONGO_URI || "mongodb://localhost:27017/ecommerce";
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(MONGO_URI);
+    
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error(`Error: ${error.message}`);
+          }
+          process.exit(1);
+    }
+    }
+
+export default connectDB;
